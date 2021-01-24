ce111b3b03df2b05c75ee130e73467e5b5bb5ba8
piyush666 <piyushgohil666@gmail.com>
2021-01-25 00:24:31 +0530

    antd table added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import Nav from '../components/nav'
+import MyTable from "../components/MyTable";
+import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
-      <div className="py-20">
+      <div className="py-20  p-4 grid ">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
+
+        <MyTable />
       </div>
     </div>
-  )
+  );
 }
